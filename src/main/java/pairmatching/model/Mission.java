9639487d4a39013010f930a027dc901952c8d0e0
khj1 <74335027+khj1@users.noreply.github.com>
2021-12-24 14:00:06 +0900

    feat: 페어 매칭 선택 시 과정, 미션, 레벨 정보를 출력하는 기능

---
+++ src/main/java/pairmatching/model/Mission.java
@@ -0,0 +1,30 @@
+package pairmatching.model;
+
+import static pairmatching.model.Level.*;
+
+public enum Mission {
+	RACING_CAR(LEVEL1, "자동차경주"),
+	LOTTO(LEVEL1, "로또"),
+	BASEBALL(LEVEL1, "숫자야구게임"),
+	BASKET(LEVEL2, "장바구니"),
+	PAYMENT(LEVEL2, "결제"),
+	PATHWAY(LEVEL2, "지하철노선도"),
+	IMPROVEMENT(LEVEL4, "성능개선"),
+	DISTRIBUTION(LEVEL4, "배포");
+
+	private final Level level;
+	private final String name;
+
+	Mission(Level level, String name) {
+		this.level = level;
+		this.name = name;
+	}
+
+	public boolean isLevel(Level level) {
+		return level.is(level);
+	}
+
+	public String getName() {
+		return name;
+	}
+}
--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,16 +1,79 @@
 package pairmatching.view;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
+import pairmatching.model.Course;
+import pairmatching.model.Level;
 import pairmatching.model.Menu;
+import pairmatching.model.Mission;
 
 public class OutputView {
 
-	public static final String DOT = ". ";
+	private static final String DOT = ". ";
+	private static final String DIVIDING_LINE = "#############################################";
+	private static final String COURSES_PREFIX = "과정: ";
+	private static final String LEVEL_PREFIX = "\t-";
+	public static final String COLON = ": ";
+	public static final String DELIMITER = " | ";
+	public static final String MISSION_PREFIX = "미션:";
 
 	public static void printCategories() {
 		printFunctionInit();
 		printFunctions();
+	}
+
+	public static void printGeneralInformation() {
+		printDividingLine();
+		printCourses();
+		printMissions();
+		printDividingLine();
+	}
+
+	private static void printMissions() {
+		printMissionPrefix();
+		printMissionsByLevel();
+
+	}
+
+	private static void printMissionsByLevel() {
+		Arrays.stream(Level.values()).forEach(level -> {
+			printLevelPrefix(level);
+			printMissionByLevel(level);
+		});
+	}
+
+	private static void printMissionByLevel(Level level) {
+		System.out.println(getMissionByLevelString(level));
+	}
+
+	private static String getMissionByLevelString(Level level) {
+		return Arrays.stream(Mission.values())
+			.filter(mission -> mission.isLevel(level))
+			.map(Mission::getName)
+			.collect(Collectors.joining(DELIMITER));
+	}
+
+	private static void printLevelPrefix(Level level) {
+		System.out.print(LEVEL_PREFIX + level.getName() + COLON);
+	}
+
+	private static void printMissionPrefix() {
+		System.out.println(MISSION_PREFIX);
+	}
+
+	private static void printCourses() {
+		System.out.print(COURSES_PREFIX + getCoursesString());
+	}
+
+	private static String getCoursesString() {
+		return Arrays.stream(Course.values())
+			.map(Course::getName)
+			.collect(Collectors.joining(DELIMITER));
+	}
+
+	private static void printDividingLine() {
+		System.out.println(DIVIDING_LINE);
 	}
 
 	private static void printFunctions() {
