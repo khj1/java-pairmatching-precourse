aab32e7c72a8fd76eb92bb707d6b03d7d524a64b
khj1 <dhepd8849@gmail.com>
2022-12-14 12:08:02 +0900

    feat: 레벨 입력 유효성 검증 추가

--- src/main/java/pairmatching/domain/Level.java
+++ src/main/java/pairmatching/domain/Level.java
@@ -1,5 +1,6 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,11 +11,23 @@ public enum Level {
     LEVEL4("레벨4", List.of(Mission.PERFORMANCE, Mission.DISTRIBUTION)),
     LEVEL5("레벨5", Collections.emptyList());
 
+    private static final String NO_SUCH_LEVEL = "존재하지 않는 레벨입니다.";
     private final List<Mission> missions;
     private final String name;
 
     Level(String name, List<Mission> missions) {
         this.name = name;
         this.missions = missions;
+    }
+
+    public static Level from(String name) {
+        return Arrays.stream(values())
+                .filter(level -> level.hasSameName(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(NO_SUCH_LEVEL));
+    }
+
+    private boolean hasSameName(String name) {
+        return this.name.equals(name);
     }
 }
