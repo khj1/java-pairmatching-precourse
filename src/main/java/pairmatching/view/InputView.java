344706491137b4b68db56b9debd762e4b93e5be9
khj1 <dhepd8849@gmail.com>
2022-12-14 12:46:18 +0900

    feat: 과정, 레벨, 미션을 입력 받는 기능 추가

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -4,9 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 import pairmatching.command.MainCommand;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputView {
+
+    private static final String REGEX = ",";
 
     public MainCommand readMainCommand() {
         printMain();
@@ -22,5 +25,12 @@ public class InputView {
         return Arrays.stream(MainCommand.values())
                 .map(MainCommand::toString)
                 .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    public List<String> readLevelCourseMission() {
+        System.out.println("과정, 레벨, 미션을 선택하세요.");
+        System.out.println("ex) 백엔드, 레벨1, 자동차경주");
+
+        return Arrays.asList(Console.readLine().split(REGEX));
     }
 }
