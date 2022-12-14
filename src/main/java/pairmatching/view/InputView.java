1a3b4e13447b55d5195fc4992b35b18110faec40
khj1 <dhepd8849@gmail.com>
2022-12-14 12:48:06 +0900

    refactor: 과정, 레벨, 미션 출력 기능을 입력 로직으로 이동

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -2,6 +2,8 @@ package pairmatching.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import pairmatching.command.MainCommand;
+import pairmatching.domain.Course;
+import pairmatching.domain.Level;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.stream.Collectors;
 public class InputView {
 
     private static final String REGEX = ",";
+    private static final String DELIMITER = " | ";
 
     public MainCommand readMainCommand() {
         printMain();
@@ -28,9 +31,44 @@ public class InputView {
     }
 
     public List<String> readLevelCourseMission() {
+        printContents();
         System.out.println("과정, 레벨, 미션을 선택하세요.");
         System.out.println("ex) 백엔드, 레벨1, 자동차경주");
 
         return Arrays.asList(Console.readLine().split(REGEX));
+    }
+
+    private void printContents() {
+        System.out.println();
+        printSeparator();
+        printCourses();
+        printMissionsByLevel();
+        printSeparator();
+    }
+
+    private static void printSeparator() {
+        System.out.println("#############################################");
+    }
+
+    private void printCourses() {
+        System.out.print("과정: ");
+        System.out.println(getCourses());
+    }
+
+    private String getCourses() {
+        return Arrays.stream(Course.values())
+                .map(Course::toString)
+                .collect(Collectors.joining(DELIMITER));
+    }
+
+    private void printMissionsByLevel() {
+        System.out.println("미션: ");
+        System.out.println(getMissionsByLevel());
+    }
+
+    private String getMissionsByLevel() {
+        return Arrays.stream(Level.values())
+                .map(Level::toString)
+                .collect(Collectors.joining(System.lineSeparator()));
     }
 }
--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,46 +1,5 @@
 package pairmatching.view;
 
-import pairmatching.domain.Course;
-import pairmatching.domain.Level;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 public class OutputView {
 
-    private static final String DELIMITER = " | ";
-
-    public void printContents() {
-        System.out.println();
-        printSeparator();
-        printCourses();
-        printMissionsByLevel();
-        printSeparator();
-    }
-
-    private static void printSeparator() {
-        System.out.println("#############################################");
-    }
-
-    private void printCourses() {
-        System.out.print("과정: ");
-        System.out.println(getCourses());
-    }
-
-    private String getCourses() {
-        return Arrays.stream(Course.values())
-                .map(Course::toString)
-                .collect(Collectors.joining(DELIMITER));
-    }
-
-    private void printMissionsByLevel() {
-        System.out.println("미션: ");
-        System.out.println(getMissionsByLevel());
-    }
-
-    private String getMissionsByLevel() {
-        return Arrays.stream(Level.values())
-                .map(Level::toString)
-                .collect(Collectors.joining(System.lineSeparator()));
-    }
 }
