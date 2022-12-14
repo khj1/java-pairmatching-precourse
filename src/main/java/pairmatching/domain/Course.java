cefd7f61f307520e652897e4f5f5704242aa8c5b
khj1 <dhepd8849@gmail.com>
2022-12-14 12:07:46 +0900

    feat: 과정 입력 유효성 검증 추가

--- src/main/java/pairmatching/domain/Course.java
+++ src/main/java/pairmatching/domain/Course.java
@@ -1,15 +1,30 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+
 public enum Course {
     BACKEND("백엔드", "src/main/resources/backend-crew.md"),
     FRONTEND("프론트엔드", "src/main/resources/frontend-crew.md");
 
+    private static final String NO_SUCH_COURSE = "존재하지 않는 고정입니다.";
+    
     private final String resource;
     private final String name;
 
     Course(String name, String resource) {
         this.name = name;
         this.resource = resource;
+    }
+
+    public static Course from(String name) {
+        return Arrays.stream(values())
+                .filter(course -> course.hasSameName(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(NO_SUCH_COURSE));
+    }
+
+    private boolean hasSameName(String name) {
+        return this.name.equals(name);
     }
 
     public String getResource() {
